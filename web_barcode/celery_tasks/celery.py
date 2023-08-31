1498720a07141ba932932be11a2d30bfe72421c1
anton1202-py <anton1202@yandex.ru>
2023-08-31 18:10:18 +0000

    server

--- web_barcode/celery_tasks/celery.py
+++ web_barcode/celery_tasks/celery.py
@@ -12,10 +12,10 @@ app.conf.beat_schedule = {
         "task": "celery_tasks.tasks.add_database_data",
         "schedule": crontab(hour=22, minute=40)
     },
-    "change-fbs": {
-        "task": "celery_tasks.tasks_yandex_fby_fbs.change_fbs_amount",
-        "schedule": crontab(hour=20, minute=40)
-    },
+   # "change-fbs": {
+    #    "task": "celery_tasks.tasks_yandex_fby_fbs.change_fbs_amount",
+     #   "schedule": crontab(hour=20, minute=40)
+   # },
     "add_fby_amount": {
         "task": "celery_tasks.tasks_yandex_fby_fbs.add_fby_amount_to_database",
         "schedule": crontab(hour=18, minute=0)
