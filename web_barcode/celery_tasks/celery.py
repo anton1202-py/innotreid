c10c668effbf2943b6ce78d017c23542725f752e
anton1202-py <anton1202@yandex.ru>
2023-09-21 08:41:18 +0000

    server

--- web_barcode/celery_tasks/celery.py
+++ web_barcode/celery_tasks/celery.py
@@ -8,9 +8,9 @@ app.config_from_object('celery_tasks.celeryconfig')
 
 
 app.conf.beat_schedule = {
-    "everyday-task": {
+    "everyday-task1": {
         "task": "celery_tasks.tasks.add_database_data",
-        "schedule": crontab(hour=9, minute=0)
+        "schedule": crontab(hour=7, minute=40)
     },
     "everyday-task": {
         "task": "celery_tasks.tasks.add_stock_data_from_frontend",
