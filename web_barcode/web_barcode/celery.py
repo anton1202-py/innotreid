4c8af48afe0a86011650c590618ae3291a0ff5e9
unknown <anton1202@yandex.ru>
2023-11-15 23:07:10 +0300

    запланированная остановка

--- web_barcode/web_barcode/celery.py
+++ web_barcode/web_barcode/celery.py
@@ -5,6 +5,6 @@ from celery import Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_barcode.settings")
 # app = Celery("web_barcode")
 app = Celery('ozon_system',
-             include=['ozon_system.tasks', 'ozon_system.my_task'])
+             include=['ozon_system.tasks'])
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
