06f337c8ef907bcd2234b4c9a1459cb25a8f009e
unknown <anton1202@yandex.ru>
2023-11-16 01:26:52 +0300

    задачи по расписанию

--- web_barcode/ozon_system/celery.py
+++ web_barcode/ozon_system/celery.py
@@ -5,5 +5,5 @@ from celery import Celery
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_barcode.settings")
 # app = Celery("web_barcode")
 app = Celery('ozon_system.tasks', broker='redis://localhost:6379/0')
-app.config_from_object("web_barcode.celeryconfig", namespace="CELERY")
+# app.config_from_object("web_barcode.celeryconfig", namespace="CELERY")
 app.autodiscover_tasks()
