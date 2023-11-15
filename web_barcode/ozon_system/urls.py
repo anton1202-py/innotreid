664504ddaa567f5124c271d016f747b1a2b7c2ca
unknown <anton1202@yandex.ru>
2023-11-16 02:10:17 +0300

    задачи по расписанию

--- web_barcode/ozon_system/urls.py
+++ web_barcode/ozon_system/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('ozon_system_main_data', views.ozon_main_info_table,
-    #     name='ozon_system_main_data'),
+    path('ozon_system_main_data', views.ozon_main_info_table,
+         name='ozon_system_main_data'),
     path('ozon_adv_group', views.ozon_adv_group,
          name='ozon_adv_group'),
     path('campaign_articles/<int:campaign_id>', views.ozon_campaing_article_info,
--- web_barcode/ozon_system/views.py
+++ web_barcode/ozon_system/views.py
@@ -8,7 +8,7 @@ import requests
 from django.db.models import Q, Sum
 from django.shortcuts import get_object_or_404, redirect, render
 from dotenv import load_dotenv
-# from ozon_system.models import ArticleAmountRating
+from ozon_system.models import ArticleAmountRating
 from ozon_system.tasks import my_task
 
 load_dotenv()
@@ -32,73 +32,73 @@ def access_token():
     return json.loads(response.text)['access_token']
 
 
-# def ozon_main_info_table(request):
-#    articles_sale_data_for_table = {}
-#    main_data = ArticleAmountRating.objects.all()
-#
-#    if request.method == 'POST' and 'show_statistic' in request.POST.keys():
-#        payload = json.dumps({
-#            "filter": {
-#                "date": {
-#                    "from": f"{request.POST['datestart']}T00:00:00.000Z",
-#                    "to": f"{request.POST['datefinish']}T00:00:00.000Z",
-#                },
-#                "operation_type": ['OperationAgentDeliveredToCustomer'],
-#                "posting_number": "",
-#                "transaction_type": "all"
-#            },
-#            "page": 1,
-#            "page_size": 1000
-#        })
-#        headers = {
-#            'Client-Id': os.getenv('CLIENT_ID'),
-#            'Api-Key': os.getenv('OZON_API_TOKEN'),
-#            'Content-Type': 'application/json',
-#        }
-#        SALE_ARTICLE_URL = "https://api-seller.ozon.ru/v3/finance/transaction/list"
-#        response = requests.request(
-#            "POST", SALE_ARTICLE_URL, headers=headers, data=payload)
-#        common_ozon_sale_data = json.loads(response.text)
-#        # print('common_ozon_sale_data', common_ozon_sale_data)
-#        article_list = []
-#        sale_data = common_ozon_sale_data['result']['operations']
-#        for i in sale_data:
-#            if i['operation_type'] == 'OperationAgentDeliveredToCustomer':
-#                article_list.append(i['items'][0]['name'])
-#
-#        raw_article_dict = Counter(article_list)
-#        raw_articles_sale_data_for_table = {}
-#
-#        for key, value in raw_article_dict.items():
-#            amount_price = 0
-#            sku = ''
-#            for article in sale_data:
-#                if article['operation_type'] == 'OperationAgentDeliveredToCustomer':
-#                    if key == article['items'][0]['name']:
-#                        amount_price += article['accruals_for_sale']
-#                        sku = article['items'][0]['sku']
-#            raw_articles_sale_data_for_table[key] = [value, amount_price, sku]
-#
-#        articles_sale_data_for_table = dict(
-#            sorted(raw_articles_sale_data_for_table.items(), key=lambda x: x[1][0], reverse=True))
-#
-#    if request.method == 'POST' and 'add_article_group' in request.POST.keys():
-#        print(request.POST)
-#        main_data = ArticleAmountRating.objects.all()
-#        for data in main_data:
-#            if data.pk % 3 == 1:
-#                data.article_group = 1
-#            elif data.pk % 3 == 2:
-#                data.article_group = 2
-#            elif data.pk % 3 == 0:
-#                data.article_group = 3
-#            data.save()
-#
-#    context = {
-#        'main_data': main_data,
-#        'table_data': articles_sale_data_for_table
-#    }
-#    return render(request, 'ozon_system/main_table.html', context)
+def ozon_main_info_table(request):
+    articles_sale_data_for_table = {}
+    main_data = ArticleAmountRating.objects.all()
+
+    if request.method == 'POST' and 'show_statistic' in request.POST.keys():
+        payload = json.dumps({
+            "filter": {
+                "date": {
+                    "from": f"{request.POST['datestart']}T00:00:00.000Z",
+                    "to": f"{request.POST['datefinish']}T00:00:00.000Z",
+                },
+                "operation_type": ['OperationAgentDeliveredToCustomer'],
+                "posting_number": "",
+                "transaction_type": "all"
+            },
+            "page": 1,
+            "page_size": 1000
+        })
+        headers = {
+            'Client-Id': os.getenv('CLIENT_ID'),
+            'Api-Key': os.getenv('OZON_API_TOKEN'),
+            'Content-Type': 'application/json',
+        }
+        SALE_ARTICLE_URL = "https://api-seller.ozon.ru/v3/finance/transaction/list"
+        response = requests.request(
+            "POST", SALE_ARTICLE_URL, headers=headers, data=payload)
+        common_ozon_sale_data = json.loads(response.text)
+        # print('common_ozon_sale_data', common_ozon_sale_data)
+        article_list = []
+        sale_data = common_ozon_sale_data['result']['operations']
+        for i in sale_data:
+            if i['operation_type'] == 'OperationAgentDeliveredToCustomer':
+                article_list.append(i['items'][0]['name'])
+
+        raw_article_dict = Counter(article_list)
+        raw_articles_sale_data_for_table = {}
+
+        for key, value in raw_article_dict.items():
+            amount_price = 0
+            sku = ''
+            for article in sale_data:
+                if article['operation_type'] == 'OperationAgentDeliveredToCustomer':
+                    if key == article['items'][0]['name']:
+                        amount_price += article['accruals_for_sale']
+                        sku = article['items'][0]['sku']
+            raw_articles_sale_data_for_table[key] = [value, amount_price, sku]
+
+        articles_sale_data_for_table = dict(
+            sorted(raw_articles_sale_data_for_table.items(), key=lambda x: x[1][0], reverse=True))
+
+    if request.method == 'POST' and 'add_article_group' in request.POST.keys():
+        print(request.POST)
+        main_data = ArticleAmountRating.objects.all()
+        for data in main_data:
+            if data.pk % 3 == 1:
+                data.article_group = 1
+            elif data.pk % 3 == 2:
+                data.article_group = 2
+            elif data.pk % 3 == 0:
+                data.article_group = 3
+            data.save()
+
+    context = {
+        'main_data': main_data,
+        'table_data': articles_sale_data_for_table
+    }
+    return render(request, 'ozon_system/main_table.html', context)
 
 
 def ozon_adv_group(request):
