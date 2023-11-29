13867c46417fdb3ca49736cdf4ffc430569f3b60
anton1202-py <anton1202@yandex.ru>
2023-11-29 15:59:07 +0300

    озон периодические задачи

--- web_barcode/ozon_system/tasks.py
+++ web_barcode/ozon_system/tasks.py
@@ -8,13 +8,12 @@ from datetime import datetime
 import pandas as pd
 import requests
 from celery import Celery, shared_task
+from celery_tasks.celery import app
 from django.db.models import Q, Sum
 from django.shortcuts import get_object_or_404, redirect, render
 from dotenv import load_dotenv
 from ozon_system.models import (AdvGroup, ArticleAmountRating, DateActionInfo,
                                 GroupActions, GroupCeleryAction, GroupCompaign)
-
-from .celery import app
 
 load_dotenv()
 
@@ -55,43 +54,10 @@ def start_compaign(compaign_id):
         "POST", url, headers=headers, data=payload_deactive)
 
 
-@shared_task
-def start_group(group_number):
-    from ozon_system.views import access_token
-    logger.info("Функция my_task приняла задание для group_number %s в %s",
-                group_number, datetime.now())
-    compaign_list = AdvCompaignGroup.objects.filter(
-        group_number=group_number).values_list('campaigns', flat=True)
-
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {access_token()}',
-    }
-    for compaign_id in compaign_list:
-        url = f"https://performance.ozon.ru:443/api/client/campaign/{compaign_id}/activate"
-        payload_active = json.dumps({
-            "campaignId": compaign_id
-        })
-        response = requests.request(
-            "POST", url, headers=headers, data=payload_active)
+@app.task
+def start_adv_company():
+    pass
 
 
-@shared_task
-def stop_group(group_number):
-    from ozon_system.views import access_token
-    logger.info("Функция my_task приняла задание для group_number %s в %s",
-                group_number, datetime.now())
-    compaign_list = AdvCompaignGroup.objects.filter(
-        group_number=group_number).values_list('campaigns', flat=True)
-
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': f'Bearer {access_token()}',
-    }
-    for compaign_id in compaign_list:
-        url = f"https://performance.ozon.ru:443/api/client/campaign/{compaign_id}/deactivate"
-        payload_deactive = json.dumps({
-            "campaignId": compaign_id
-        })
-        response = requests.request(
-            "POST", url, headers=headers, data=payload_deactive)
+def stop_adv_company():
+    pass
