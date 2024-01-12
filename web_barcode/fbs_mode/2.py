ec405b56d98526a9b26ba7a211e8f814ad9821e4
unknown <anton1202@yandex.ru>
2024-01-12 23:46:50 +0300

    1

--- web_barcode/fbs_mode/2.py
+++ web_barcode/fbs_mode/2.py
@@ -2,7 +2,7 @@ import os
 
 from msoffice2pdf import convert
 
-filename = "web_barcode/fbs_mode/data_for_barcodes/pivot_excel/На производство ИП 10.01.2024 22-10-45.xlsx"
+filename = "web_barcode/fbs_mode/data_for_barcodes/pivot_excel/file.xlsx"
 full_path = os.path.abspath(filename)
 folder_path = os.path.dirname(os.path.abspath(full_path))
 new_pdf_name = f"{folder_path}/new.pdf"
