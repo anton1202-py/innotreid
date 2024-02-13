9ab849e5acfc029013bde8407bf0c0d3d5d48a0e
unknown <anton1202@yandex.ru>
2024-02-14 00:52:34 +0300

    test

--- web_barcode/fbs_mode/2.py
+++ web_barcode/fbs_mode/2.py
@@ -157,8 +157,9 @@ def create_ozone_selection_sheet_pdf(fbs_ozon_common_data_buils_dict):
             c[1].border = Border(top=thin, left=thin,
                                  bottom=thin, right=thin)
             wrapped_lines = textwrap.wrap(create.cell(
-                row=i+2, column=1).value, width=38)
+                row=i+2, column=2).value, width=38)
             num_lines = len(wrapped_lines)
+            print('num_lines', num_lines)
             row_height = 12 * num_lines
             print('row_height', row_height)
 
