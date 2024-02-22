2da1f44412eed00a45370b1de30f9dceda5cdc67
unknown <anton1202@yandex.ru>
2024-02-22 15:17:01 +0300

    ozon first

--- web_barcode/fbs_mode/1.py
+++ web_barcode/fbs_mode/1.py
@@ -1,11 +1,57 @@
-import datetime
+import json
+import os
 
-# Получаем текущую дату
-today = datetime.datetime.now()
+import requests
+from dotenv import load_dotenv
 
-print(today.hour)
+dotenv_path = os.path.join(os.path.dirname(
+    __file__), '..', 'web_barcode', '.env')
+load_dotenv(dotenv_path)
+API_KEY_WB_IP = os.getenv('API_KEY_WB_IP')
+YANDEX_IP_KEY = os.getenv('YANDEX_IP_KEY')
+API_KEY_OZON_KARAVAEV = os.getenv('API_KEY_OZON_KARAVAEV')
+CLIENT_ID_OZON_KARAVAEV = os.getenv('CLIENT_ID_OZON_KARAVAEV')
 
-# Используем метод strftime() для форматирования даты и вывода дня недели
-day_of_week = today.strftime('%A')
+OZON_OOO_API_TOKEN = os.getenv('OZON_OOO_API_TOKEN')
+OZON_OOO_CLIENT_ID = os.getenv('OZON_OOO_CLIENT_ID')
+YANDEX_OOO_KEY = os.getenv('YANDEX_OOO_KEY')
+WB_OOO_API_KEY = os.getenv('WB_OOO_API_KEY')
 
-print("Сегодня " + day_of_week)
+
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+CHAT_ID_ADMIN = os.getenv('CHAT_ID_ADMIN')
+CHAT_ID_MANAGER = os.getenv('CHAT_ID_MANAGER')
+CHAT_ID_EU = os.getenv('CHAT_ID_EU')
+CHAT_ID_AN = os.getenv('CHAT_ID_AN')
+
+
+wb_headers_karavaev = {
+    'Content-Type': 'application/json',
+    'Authorization': API_KEY_WB_IP
+}
+wb_headers_ooo = {
+    'Content-Type': 'application/json',
+    'Authorization': WB_OOO_API_KEY
+}
+
+ozon_headers_karavaev = {
+    'Api-Key': API_KEY_OZON_KARAVAEV,
+    'Content-Type': 'application/json',
+    'Client-Id': CLIENT_ID_OZON_KARAVAEV
+}
+ozon_headers_ooo = {
+    'Api-Key': OZON_OOO_API_TOKEN,
+    'Content-Type': 'application/json',
+    'Client-Id': OZON_OOO_CLIENT_ID
+}
+
+url_data = 'https://suppliers-api.wildberries.ru/api/v3/supplies?limit=2&next=10'
+
+
+payload = json.dumps(
+    {}
+)
+response_data = requests.request(
+    "GET", url_data, headers=wb_headers_karavaev)
+print(response_data.status_code)
+print(response_data.text)
