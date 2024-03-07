c72b7e75392e93c76b1cad92f63b21642d17abc5
unknown <anton1202@yandex.ru>
2024-03-07 19:19:31 +0300

    система с ценами

--- web_barcode/price_system/templatetags/price_system_util.py
+++ web_barcode/price_system/templatetags/price_system_util.py
@@ -16,3 +16,19 @@ def integer(value):
 @register.filter
 def get_item(dictionary, key):
     return dictionary.get(key)
+
+@register.filter
+def multiplicity(value, arg):
+    return value * arg
+
+@register.filter
+def divide(value, arg):
+    return value / arg
+
+@register.filter
+def sub(value, arg):
+    return value - arg
+
+@register.filter
+def add(value, arg):
+    return value + arg
