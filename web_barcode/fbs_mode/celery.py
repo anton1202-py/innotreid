aca32e2f08fa2ff6408bd362b554d74f87554d97
unknown <anton1202@yandex.ru>
2024-03-14 22:03:20 +0300

    ce;ery errors

--- web_barcode/fbs_mode/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from celery import Celery
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_barcode.settings")
-
-app = Celery('fbs_mode.tasks', broker='redis://localhost:6379/0')
-app.autodiscover_tasks()
