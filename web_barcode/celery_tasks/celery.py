992934cfaed9c8e8b17fbff76e59b137b46af871
unknown <anton1202@yandex.ru>
2024-04-01 21:55:31 +0300

    изменил время сборки

--- web_barcode/celery_tasks/celery.py
+++ web_barcode/celery_tasks/celery.py
@@ -74,7 +74,7 @@ app.conf.beat_schedule = {
     },
     "ooo_fbs_action": {
         "task": "fbs_mode.tasks.ooo_common_task",
-        "schedule": crontab(hour=18, minute=47)
+        "schedule": crontab(hour=17, minute=40)
     },
     "ip_fbs_morning_action": {
         "task": "fbs_mode.tasks.ip_morning_task",
