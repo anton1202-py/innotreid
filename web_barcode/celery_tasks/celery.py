cd35ac111e000a6e1eab55062f7922801c51e885
unknown <anton1202@yandex.ru>
2024-04-03 11:17:09 +0300

    Загружать ценовые группы из excel файла

--- web_barcode/celery_tasks/celery.py
+++ web_barcode/celery_tasks/celery.py
@@ -128,7 +128,7 @@ app.conf.beat_schedule = {
 
     "wb_reklama_campaign_budget_add": {
         "task": "reklama.periodic_tasks.budget_working",
-        "schedule": crontab(hour=7, minute=21)
+        "schedule": crontab(hour=21, minute=1)
     },
     "wb_ooo_article_add_to_db": {
         "task": "reklama.periodic_tasks.ooo_wb_articles_data",
