4a5ae13797cb9afa2b50deae68ce099ea120bd87
unknown <anton1202@yandex.ru>
2024-04-05 11:18:31 +0300

    reklama. Сравниваю кампании и артикулы

--- web_barcode/celery_tasks/celery.py
+++ web_barcode/celery_tasks/celery.py
@@ -136,7 +136,7 @@ app.conf.beat_schedule = {
     },
     "wb_ooo_matching_article_campaign": {
         "task": "reklama.periodic_tasks.matching_wb_ooo_article_campaign",
-        "schedule": crontab(hour=22, minute=3)
+        "schedule": crontab(hour=8, minute=22)
     },
     "wb_ooo_stock_fbo": {
         "task": "reklama.periodic_tasks.wb_ooo_fbo_stock_count",
