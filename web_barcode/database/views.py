12198cd0a0c046ee8f3a20adc4c2d4d3a064d504
unknown <anton1202@yandex.ru>
2024-05-06 10:45:12 +0300

    ошибка после доьбавления данных с ВБ

--- web_barcode/database/views.py
+++ web_barcode/database/views.py
@@ -108,7 +108,7 @@ START_LIST = [
 def database_home(request):
     if str(request.user) == 'AnonymousUser':
         return redirect('login')
-    # process_wb_sales_data()
+    process_wb_sales_data()
     # save_wildberries_sale_data_for_motivation()
     data = Articles.objects.all()
     context = {
