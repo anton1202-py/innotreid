e3f33bafd7c7f5bf5a8369433e0f3643c75ac4eb
unknown <anton1202@yandex.ru>
2024-06-08 11:45:51 +0300

    сдвиг сборки фбс

--- web_barcode/celery_tasks/celery.py
+++ web_barcode/celery_tasks/celery.py
@@ -116,7 +116,7 @@ app.conf.beat_schedule = {
     },
     "ip_fbs_morning_action": {
         "task": "fbs_mode.tasks.ip_morning_task",
-        "schedule": crontab(hour=8, minute=20)
+        "schedule": crontab(hour=8, minute=52)
     },
     "ip_ozon_day": {
         "task": "fbs_mode.tasks.ip_ozon_action_day",
