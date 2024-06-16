992d4188ca224493060a2f9f8c7b31b1a84a72ca
unknown <anton1202@yandex.ru>
2024-06-16 21:26:30 +0300

    excel файл вознаграждения. Отстройка ошибки

--- web_barcode/motivation/views.py
+++ web_barcode/motivation/views.py
@@ -559,12 +559,13 @@ def download_designer_rewards_excel(request):
     article_list = request.POST.get('article_list')
     cleaned_string = article_list.strip("<QuerySet").strip(">")
 
-    article_list = cleaned_string
+    article_list = ast.literal_eval(cleaned_string)
     main_sales_dict = ast.literal_eval(request.POST.get('main_sales_dict'))
     designer_id = int(request.POST.get('designer_id'))
     sales_year = int(request.POST.get('sales_year'))
     month_list = ast.literal_eval(request.POST.get('month_list'))
     user_data = InnotreidUser.objects.get(id=designer_id)
+
     response = HttpResponse(
         content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
     response['Content-Disposition'] = f'attachment; filename="{user_data.last_name}_{user_data.first_name}_rewards.xlsx"'
