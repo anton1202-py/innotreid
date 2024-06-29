0a676aebdff4ebf4ca9eca829842122b49fbc214
unknown <anton1202@yandex.ru>
2024-06-29 15:37:38 +0300

    Перенес модуль пополнения рекламных кампаний в созданные кампании

--- web_barcode/create_reklama/views.py
+++ web_barcode/create_reklama/views.py
@@ -126,9 +126,6 @@ def campaigns_were_created_with_system(request):
             update_campaign_status()
             return redirect('campaigns_list')
 
-        elif 'change-button' in request.POST:
-            print(request.POST)
-    print(koef_dict)
     context = {
         'page_name': page_name,
         'campaigns_list': campaigns_list,
