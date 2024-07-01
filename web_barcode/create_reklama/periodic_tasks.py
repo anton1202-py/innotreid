702b84a2c47948fec9f6e5cb7b44b98f525c7a0a
unknown <anton1202@yandex.ru>
2024-07-01 20:15:54 +0300

    перевод статистики на новый модуль

--- web_barcode/create_reklama/periodic_tasks.py
+++ web_barcode/create_reklama/periodic_tasks.py
@@ -109,6 +109,7 @@ def update_campaign_status():
                             campaign_obj.campaign_status = campaign_status
                             campaign_obj.save()
                         elif int(campaign_obj.campaign_number) in campaign_list and campaign_status == 7:
+                            print(campaign_obj)
                             campaign_obj.delete()
 
 
