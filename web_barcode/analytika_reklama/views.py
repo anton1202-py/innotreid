c72c93f1f23c853aeb0063694184fff3ad117a1a
unknown <anton1202@yandex.ru>
2024-07-06 13:28:10 +0300

    статитстика ключевой фразы

--- web_barcode/analytika_reklama/views.py
+++ web_barcode/analytika_reklama/views.py
@@ -78,9 +78,9 @@ def common_adv_statistic(request):
 @login_required
 def keyword_statistic_info(request):
     """Отображает статистику ключевых фраз"""
-    page_name = 'Статистика ключевых фраз (более 300 показов)'
+    page_name = 'Статистика ключевых фраз (более 100 показов)'
 
-    keyword_stats = StatisticCampaignKeywordPhrase.objects.filter(views__gt=10).values('keyword__phrase').annotate(
+    keyword_stats = StatisticCampaignKeywordPhrase.objects.filter(views__gt=100).values('keyword__phrase').annotate(
         keyword_obj=F('keyword'),
         total_views=Sum('views'),
         total_clicks=Sum('clicks'),
